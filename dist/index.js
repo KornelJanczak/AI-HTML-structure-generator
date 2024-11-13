8adb8b116bd8ecc3d1a86560972202a19682e2f2
KornelJanczak <korneljanczak10@gmail.com>
2024-11-13 16:46:45 +0100

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Main {
+    constructor() {
+        console.log("Hello World");
+    }
+    hello() {
+        console.log("hello");
+    }
+}
+const main = new Main();
+main.hello();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+class Main {
+  constructor() {
+    console.log("Hello World");
+  }
+
+  hello(): void {
+    console.log("hello");
+  }
+}
+
+const main = new Main();
+
+main.hello();
