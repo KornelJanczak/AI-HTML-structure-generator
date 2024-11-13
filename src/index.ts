28dddaed2c868696daafb36a02890b4c3b888d45
KornelJanczak <korneljanczak10@gmail.com>
2024-11-13 19:57:56 +0100

    feat: enhance file processing by adding AI text generation and result saving functionality

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import "dotenv";
+import dotenv from "dotenv";
+// import openai from "./ai";
 import { openai } from "@ai-sdk/openai";
-import { CoreMessage, streamText, tool } from "ai";
-import { readFile } from "fs/promises";
+import {
+  CoreMessage,
+  generateText,
+  streamText,
+  tool,
+  GenerateTextResult,
+  CoreTool,
+} from "ai";
+import { readFile, writeFile } from "fs/promises";
 import ServerError from "./serverError";
 import path from "path";
+import { articlePrompt } from "./ai/prompts";
+
+dotenv.config();
 
 interface MainAbstract {
   execute(): Promise<void>;
@@ -11,23 +22,69 @@ interface MainAbstract {
 
 class Main implements MainAbstract {
   async execute(): Promise<void> {
-    this.readTxtFile();
+    const txtFileContent = await this.readTxtFile();
+    const generatedResult = await this.sendTxtFileContentToAI(txtFileContent);
+    console.log("Result", generatedResult);
+    await this.saveResultToFile(generatedResult);
   }
 
-  private async readTxtFile(): Promise<void> {
+  private async readTxtFile(): Promise<string> {
     const filePath = path.join(__dirname, "../artykul-content.txt");
 
+    let fileText: string;
+
     try {
-      const fileText = await readFile(filePath, {
+      fileText = await readFile(filePath, {
         encoding: "utf-8",
       });
-
-      console.log(fileText);
     } catch (err) {
       console.error(err);
       throw new ServerError({
         name: "File System Error",
         message: "Error while reading file",
+        code: 500,
+        stack: err.stack,
+      });
+    }
+
+    return fileText;
+  }
+
+  private async sendTxtFileContentToAI(fileContent: string): Promise<string> {
+    let result: GenerateTextResult<Record<string, CoreTool<any, any>>>;
+
+    try {
+      result = await generateText({
+        model: openai("gpt-4-turbo"),
+        system: articlePrompt,
+        prompt: fileContent,
+      });
+    } catch (err) {
+      console.error(err);
+      throw new ServerError({
+        name: "AI Error",
+        message: "Error while generating text",
+        code: 500,
+        stack: err.stack,
+      });
+    }
+
+    return result.text;
+  }
+
+  private async saveResultToFile(result: string): Promise<void> {
+    const filePath = path.join(__dirname, "../artykul.html");
+
+    try {
+      await writeFile(filePath, result, {
+        encoding: "utf-8",
+      });
+      console.log("Result saved to file:", filePath);
+    } catch (err) {
+      console.error(err);
+      throw new ServerError({
+        name: "File System Error",
+        message: "Error while writing to file",
         code: 500,
         stack: err.stack,
       });
