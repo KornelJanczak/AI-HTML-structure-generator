233598ed3bc11833e8b5f9ece3298c798dbadb20
KornelJanczak <korneljanczak10@gmail.com>
2024-11-13 18:16:29 +0100

    feat: implement file reading functionality with error handling

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
-class Main {
-  constructor() {
-    console.log("Hello World");
+import "dotenv";
+import { openai } from "@ai-sdk/openai";
+import { CoreMessage, streamText, tool } from "ai";
+import { readFile } from "fs/promises";
+import ServerError from "./serverErrot";
+import path from "path";
+
+interface MainAbstract {
+  execute(): Promise<void>;
+}
+
+class Main implements MainAbstract {
+  async execute(): Promise<void> {
+    this.readTxtFile();
   }
 
-  hello(): void {
-    console.log("hello");
+  private async readTxtFile(): Promise<void> {
+    const filePath = path.join(__dirname, "../artykul-content.txt");
+
+    try {
+      const fileText = await readFile(filePath, {
+        encoding: "utf-8",
+      });
+
+      console.log(fileText);
+    } catch (err) {
+      console.error(err);
+      throw new ServerError({
+        name: "File System Error",
+        message: "Error while reading file",
+        code: 500,
+        stack: err.stack,
+      });
+    }
   }
 }
 
 const main = new Main();
-
-main.hello();
+main.execute();
