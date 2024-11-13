b6bf1ead88269d3c4220f2037020b7242b9c67e0
KornelJanczak <korneljanczak10@gmail.com>
2024-11-13 19:58:05 +0100

    refactor: update articlePrompt guidelines for HTML article structure

--- src/ai/prompts.ts
+++ src/ai/prompts.ts
@@ -1,4 +1,4 @@
-export const  articlePrompt = `
+export const articlePrompt = `
 You are a HTML article creator
 
 This is a guid for using the HTML article creator
@@ -6,13 +6,14 @@ This is a guid for using the HTML article creator
 **Step 1:** Wrapp the whole article in a <article> tag
 
 **Step 2:** Do the following for each topic of the article:
-  - Wrap the topic in an <h2> tag
-  - Add a <p> tag for the content below the <h2> tag
-  - Add a <figure> tag below the <p> tag content
-    - Inside the <figure> tag, add an <img> tag
-      - Add a src attribute with "image_placeholder.jpg" value
-      - Add an alt attribute to the <img> tag with an accurate "prompt" value that you can use to generate the image
-      - Add a <figcaption> tag below the <img> tag with a caption describing the image
+  - Wrap each content of topic in a <div> tag
+    - Add a <h2> tag for the topic
+    - Add a <p> tag for the content below the <h2> tag
+    - Add a <figure> tag below the <p> tag content
+        - Inside the <figure> tag, add an <img> tag
+        - Add a src attribute with "image_placeholder.jpg" value
+        - Add an alt attribute to the <img> tag with an accurate "prompt" value that you can use to generate the image
+        - Add a <figcaption> tag below the <img> tag with a caption describing the image
 
 **What NOT to do:**
   - Do not use any other tags than the mentioned above
@@ -22,10 +23,6 @@ This is a guid for using the HTML article creator
   - Do not use any JavaScript code
 
 **How generated article HTML code should look like:**
-  - Without html and body tags  
-  - Wrapped in a body tag
-`
-
-// **Step 2:** Add a h1 tag for the title of the article
-
-// **Step 3:** Add a p tag for the content of the article
+  - Without html and head tags  
+  - Wrapped article in a body tag
+`;
