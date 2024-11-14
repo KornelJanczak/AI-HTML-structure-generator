a6b9ef954df0499f15363af1f3f91e21c0520739
KornelJanczak <korneljanczak10@gmail.com>
2024-11-14 21:17:51 +0100

    refactor: restructure Main class to use AIService and FileService for improved modularity

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,34 @@
 import dotenv from "dotenv";
-// import openai from "./ai";
-import { openai } from "@ai-sdk/openai";
-import {
-  CoreMessage,
-  generateText,
-  streamText,
-  tool,
-  GenerateTextResult,
-  CoreTool,
-} from "ai";
-import { readFile, writeFile } from "fs/promises";
-import ServerError from "./serverError";
-import path from "path";
-import { articlePrompt } from "./ai/prompts";
+import { IAIService } from "./services/AIService";
+import { IFileService } from "./services/fileService";
+import aiService from "./services/AIService";
+import fileService from "./services/fileService";
 
 dotenv.config();
 
-interface MainAbstract {
+interface IMain {
   execute(): Promise<void>;
 }
 
-class Main implements MainAbstract {
+class Main implements IMain {
+  AIService: IAIService;
+  fileService: IFileService;
+
+  constructor(AIService: IAIService, fileService: IFileService) {
+    this.AIService = AIService;
+    this.fileService = fileService;
+  }
+
   async execute(): Promise<void> {
-    const txtFileContent = await this.readTxtFile();
-    const generatedResult = await this.sendTxtFileContentToAI(txtFileContent);
-    console.log("Result", generatedResult);
-    await this.saveResultToFile(generatedResult);
-  }
+    const txtFileContent = await this.fileService.readTxtFile();
+    const htmlContent = await this.AIService.sendTxtFileContentToAI(
+      txtFileContent
+    );
+    await this.fileService.saveResultToFile(htmlContent);
 
-  private async readTxtFile(): Promise<string> {
-    const filePath = path.join(__dirname, "../artykul-content.txt");
-
-    let fileText: string;
-
-    try {
-      fileText = await readFile(filePath, {
-        encoding: "utf-8",
-      });
-    } catch (err) {
-      console.error(err);
-      throw new ServerError({
-        name: "File System Error",
-        message: "Error while reading file",
-        code: 500,
-        stack: err.stack,
-      });
-    }
-
-    return fileText;
-  }
-
-  private async sendTxtFileContentToAI(fileContent: string): Promise<string> {
-    let result: GenerateTextResult<Record<string, CoreTool<any, any>>>;
-
-    try {
-      result = await generateText({
-        model: openai("gpt-4-turbo"),
-        system: articlePrompt,
-        prompt: fileContent,
-      });
-    } catch (err) {
-      console.error(err);
-      throw new ServerError({
-        name: "AI Error",
-        message: "Error while generating text",
-        code: 500,
-        stack: err.stack,
-      });
-    }
-
-    return result.text;
-  }
-
-  private async saveResultToFile(result: string): Promise<void> {
-    const filePath = path.join(__dirname, "../artykul.html");
-
-    try {
-      await writeFile(filePath, result, {
-        encoding: "utf-8",
-      });
-      console.log("Result saved to file:", filePath);
-    } catch (err) {
-      console.error(err);
-      throw new ServerError({
-        name: "File System Error",
-        message: "Error while writing to file",
-        code: 500,
-        stack: err.stack,
-      });
-    }
+    console.log("Result: ", htmlContent);
   }
 }
 
-const main = new Main();
+const main = new Main(aiService, fileService);
 main.execute();
