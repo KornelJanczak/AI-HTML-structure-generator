3db21a753770a7796d7e4430dbb5f0e1c035eb70
KornelJanczak <korneljanczak10@gmail.com>
2024-11-14 21:17:42 +0100

    feat: implement file service for reading and saving text files

---
+++ src/services/fileService.ts
@@ -0,0 +1,54 @@
+import { readFile, writeFile } from "fs/promises";
+import ServerError from "../serverError";
+import path from "path";
+
+export interface IFileService {
+  readTxtFile(): Promise<string>;
+  saveResultToFile(result: string): Promise<void>;
+}
+
+class FileService implements IFileService {
+  async readTxtFile(): Promise<string> {
+    const filePath = path.join(__dirname, "../artykul-content.txt");
+
+    let fileText: string;
+
+    try {
+      fileText = await readFile(filePath, {
+        encoding: "utf-8",
+      });
+    } catch (err) {
+      console.error(err);
+      throw new ServerError({
+        name: "File System Error",
+        message: "Error while reading file",
+        code: 500,
+        stack: err.stack,
+      });
+    }
+
+    return fileText;
+  }
+
+  async saveResultToFile(result: string): Promise<void> {
+    const filePath = path.join(__dirname, "../artykul.html");
+
+    try {
+      await writeFile(filePath, result, {
+        encoding: "utf-8",
+      });
+      console.log("Result saved to file:", filePath);
+    } catch (err) {
+      console.error(err);
+      throw new ServerError({
+        name: "File System Error",
+        message: "Error while writing to file",
+        code: 500,
+        stack: err.stack,
+      });
+    }
+  }
+}
+
+const fileService = new FileService();
+export default fileService;
