10bc165670a78b365314c7841fb4df62385d435b
KornelJanczak <korneljanczak10@gmail.com>
2024-11-15 22:19:46 +0100

    feat: integrate logging into AI service for improved error tracking

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -1,7 +1,8 @@
 import { openai } from "@ai-sdk/openai";
 import { generateText, GenerateTextResult, CoreTool } from "ai";
-import ServerError from "../serverError";
-import { articlePrompt } from "../ai/prompts";
+import ServerError from "../common/errors/serverError";
+import { articlePrompt } from "../common/ai/prompts";
+import logger from "../common/logger";
 
 export interface IAIService {
   sendTxtFileContentToAI(fileContent: string): Promise<string>;
@@ -10,15 +11,18 @@ export interface IAIService {
 class AIService implements IAIService {
   async sendTxtFileContentToAI(fileContent: string): Promise<string> {
     try {
+      logger.info("Sending file content to AI service...");
       const result: GenerateTextResult<Record<string, CoreTool<any, any>>> =
         await generateText({
           model: openai("gpt-4-turbo"),
           system: articlePrompt,
           prompt: fileContent,
         });
+
+      logger.info("AI service response received.");
       return result.text;
     } catch (err) {
-      console.error("AI Service Error:", err);
+      logger.error("AI Service Error:", err);
       throw new ServerError({
         name: "AI Error",
         message: "Error while generating text",
